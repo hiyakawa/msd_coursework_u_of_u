f4cfca78baa6903bc90f9af193706de9bf4253ca
Laura <u1430770@utah.edu>
2022-08-26 18:54:11 -0600

    day 5 commit

--- Day2/Average/Average/main.cpp
+++ Day2/Average/Average/main.cpp
@@ -3,29 +3,27 @@
 //  Average
 //
 //  Created by Reshma Raghavan on 8/23/22.
-//  Group members: Laura Zhang and Reshma Raghavan
+//  Group members: Laura Zhang, Reshma Raghavan
 //  This is a program to calculate the average of a number of values
 //  in this case, it is used to calculate the average of grades
 
 #include <iostream>
 
-int main(int argc, const char * argv[])
-{
+using namespace std;
+
+int main() {
     int assignmentScore1; int assignmentScore2; int assignmentScore3;
     int assignmentScore4; int assignmentScore5;
-    std::cout << "Enter five assignment scores: " << std::endl;
     
-    std::cin >> assignmentScore1;
-    std::cin >> assignmentScore2;
-    std::cin >> assignmentScore3;
-    std::cin >> assignmentScore4;
-    std::cin >> assignmentScore5;
+    cout << "Enter five assignment scores: " << endl;
+    
+    cin >> assignmentScore1 >> assignmentScore2 >> assignmentScore3 >> assignmentScore4 >> assignmentScore5;
 
     int totalScore = assignmentScore1 + assignmentScore2 + assignmentScore3 + assignmentScore4 + assignmentScore5;
     
     float scoreAverage = totalScore / 5.0;
     
-    std::cout << "The average score of the five assignments is " << scoreAverage << std::endl;
+    cout << "The average score of the five assignments is " << scoreAverage << endl;
     
     return 0;
 }
--- day1/README.md
+++
@@ -1,9 +0,0 @@
-# CS 6010 Day 1
-
-Muyuan Zhang
-
-This folder contains:
-
-Lab1: Set up Github
-
-Lab2: Hello World
