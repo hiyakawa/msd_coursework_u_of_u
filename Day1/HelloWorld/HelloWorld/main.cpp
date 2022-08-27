5685aa5a1691dc8b98a14cb4e4541862bb44e9ff
Laura <u1430770@utah.edu>
2022-08-26 18:55:11 -0600

    day 5 commit

---
+++ Day1/HelloWorld/HelloWorld/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  HelloWorld
+//
+//  Created by Laura Zhang on 8/22/22.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    std::cout << (2 + 2);
+    return 0;
+}
---
+++ Day1/README.md
@@ -0,0 +1,9 @@
+# CS 6010 Day 1
+
+Muyuan Zhang
+
+This folder contains:
+
+Lab1: Set up Github
+
+Lab2: Hello World
