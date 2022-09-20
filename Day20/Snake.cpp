3321b6f370389c597d0916a8eb12bd4409829991
“Mark <u1373495@utah.edu>
2022-09-19 22:27:30 -0600

    Added a Snake class

---
+++ Day20/Snake.cpp
@@ -0,0 +1,8 @@
+//
+//  Snake.cpp
+//  SnakeGame
+//
+//  Created by Mark Hale on 9/19/22.
+//
+
+#include "Snake.hpp"
---
+++ Day20/Snake.hpp
@@ -0,0 +1,13 @@
+//
+//  Snake.hpp
+//  SnakeGame
+//
+//  Created by Mark Hale on 9/19/22.
+//
+
+#ifndef Snake_hpp
+#define Snake_hpp
+
+#include <stdio.h>
+
+#endif /* Snake_hpp */
