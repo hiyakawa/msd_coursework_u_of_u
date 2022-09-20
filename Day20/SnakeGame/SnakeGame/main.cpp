8ef5a8c12dfa1418fd13b97957f6b45cb54b7107
“Mark <u1373495@utah.edu>
2022-09-19 21:41:36 -0600

    Added lab code which projects a window and circle object

--- Day20/SnakeGame/SnakeGame/main.cpp
+++ Day20/SnakeGame/SnakeGame/main.cpp
@@ -9,6 +9,7 @@
 
 #include <curses.h>
 #include <iostream>
+#include <SFML/Graphics.hpp>
 
 using namespace std;
 
@@ -71,6 +72,33 @@ int main(int argc, const char * argv[]) {
         break;
     }
      */
+    
+    //Laura, this is code from Day18 lab:
+    
+    // create the window
+    sf::RenderWindow window(sf::VideoMode(800, 600), "My window");
+    // run the program as long as the window is open
+    while (window.isOpen())
+    {
+        // check all the window's events that were triggered since the
+        //last
+        //iteration of the loop
+        sf::Event event;
+        while (window.pollEvent(event))
+        {
+            // "close requested" event: we close the window
+            if (event.type == sf::Event::Closed)
+                window.close();
+        }
+        // clear the window with black color
+        window.clear(sf::Color::Black);
+        sf::CircleShape shape(50.f);
+        // set the shape color to green
+        shape.setFillColor(sf::Color(100, 250, 50));
+        window.draw(shape);
+        // end the current frame
+        window.display();
+    }
     
     return 0;
 }
