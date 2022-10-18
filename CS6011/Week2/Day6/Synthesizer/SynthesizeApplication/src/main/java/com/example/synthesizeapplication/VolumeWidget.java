913fa54cdc00d7cd0dc457077a9b0cbfb3f48df7
Laura <u1430770@utah.edu>
2022-10-17 21:08:01 -0600

    day 11 commit:

---
+++ CS6011/Week2/Day6/Synthesizer/SynthesizeApplication/src/main/java/com/example/synthesizeapplication/VolumeWidget.java
@@ -0,0 +1,2 @@
+package com.example.synthesizeapplication;public class VolumeWidget {
+}
---
+++ CS6011/Week3/Day11/HelloJS/helloJS.js
@@ -0,0 +1,30 @@
+document.writeln("Hello world from document.writeln()!")
+// will show in the web page
+console.log("Hello world from console.log()!")
+// will show in the console
+
+let myArray = ["String", true, 3, 2.0,
+    {"city": "salt lake city", "country": "U.S."}];
+
+console.log(myArray);
+
+for (let val of myArray) {
+    console.log(val);
+}
+
+function addTwoNum(a, b) {
+    return a + b;
+}
+
+let myFunction = function(a, b) {
+    return a + b;
+}
+// for the "let" version, the function do not have a name
+
+console.log(addTwoNum(1.1, 2));
+// float + int -> float
+console.log(myFunction(1.01, "2"));
+// returns "1.012"
+// int + string -> string
+
+
