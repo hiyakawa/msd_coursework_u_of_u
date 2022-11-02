515f476a241146f4a3ef9a090d0ffa8d42d9285d
Laura <u1430770@utah.edu>
2022-11-02 00:54:25 -0600

    day 22 commit

--- CS6011/Week5/Day22/app/src/main/java/com/example/helloandroid/MainActivity.java
+++ CS6011/Week5/Day22/app/src/main/java/com/example/helloandroid/MainActivity.java
@@ -3,6 +3,9 @@ package com.example.helloandroid;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void handleClick(View view) {
+        Log.i("Dd:MainActivity", "button was pressed");
+        sendMessage(view);
+    }
+
+    public void sendMessage(View view){
+        // Do something in response to button click
+        TextView textView =  findViewById(R.id.textview);
+        textView.setText("Hello world from Android!");
     }
 }
