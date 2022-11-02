2c4880f22cd6d2d1f43a33b8fc9528ff352bcbb1
Laura <u1430770@utah.edu>
2022-11-02 11:09:32 -0600

    day 23 commit

--- CS6011/Week4/Day17/HelloThreads/src/Main.java
+++ CS6011/Week4/Day17/HelloThreads/src/Main.java
@@ -1,9 +1,67 @@
 public class Main {
-    public static void sayHello() {
+    public static int answer_ = 0;
 
+    public static void sayHello() throws InterruptedException {
+        int threadSize = 10;
+
+        Thread[] myThreads = new Thread[threadSize];
+
+        for (int i = 0; i < threadSize; i++) {
+            MyRunnable runnable = new MyRunnable();
+            Thread myThread = new Thread(runnable);
+            myThread.start();
+            myThreads[i] = myThread;
+        }
+
+        for (int j = 0; j < myThreads.length; j++) {
+            myThreads[j].join();
+        }
     }
 
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void badSum() throws InterruptedException {
+        int maxValue = 100;
+
+        Thread[] myThreads = new Thread[10];
+        int numThreads = myThreads.length;
+        for (int i = 0; i < numThreads; i++) {
+            final int finalI = i;
+
+            // make a thread that uses finalI inside it's runnable
+            Thread thread = new Thread(() -> {
+                for (int j = (finalI * maxValue / numThreads);
+                     j < Math.min((finalI + 1) * maxValue / numThreads, maxValue);
+                     j++) {
+                    answer_ += j;
+                }
+            });
+
+            myThreads[i] = thread;
+            myThreads[i].start();
+        }
+
+        for (Thread myThread : myThreads) {
+            myThread.join();
+        }
+
+        int correctAnswer = maxValue * (maxValue - 1) / 2;
+        System.out.println("Correct answer: " + correctAnswer);
+        System.out.println("Bad answer: " + answer_);
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        sayHello();
+        /*
+         * Q: Do all the threads run in order?
+         * A: No!
+         *
+         * Q: Does the same thread always print the 1st hello? The last hello?
+         * A: No...?
+         */
+
+        badSum();
+        /*
+         * Q: Try setting maxValue to 100. Run you program multiple times. What do you see? Why?
+         * A: The bad answer is always equal to the correct answer.
+         */
     }
 }
--- CS6011/Week4/Day17/HelloThreads/src/MyRunnable.java
+++ CS6011/Week4/Day17/HelloThreads/src/MyRunnable.java
@@ -1,10 +1,18 @@
 public class MyRunnable implements Runnable {
     @Override
     public void run() {
-        int count = (int) (Thread.currentThread().getID());
-        while (count < 100) {
-            System.out.println("hello number " + count + " from thread number ");
+//        int count = (int) (Thread.currentThread().getId() * 100);
+        int count = 0;
+
+        while (count <= 100) {
+            if (count % 10 == 9) {
+                System.out.println("");
+            }
+
+            System.out.print("Hello number " + count + " from thread number " + Thread.currentThread().getId() + "    ");
             count++;
         }
+
+        System.out.println("\n");
     }
 }
